960e25657e2dcd8bffc70fb9dcc0d7a14216d020
Nexus-w1b <visi4ura@gmail.com>
2026-02-07 13:11:10 +0700

    Create login.js

---
+++ login.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBK-MehTTzURY5jT5stH363Y5_WE4iy-XA",
+    authDomain: "volca-tools.firebaseapp.com",
+    projectId: "volca-tools",
+    storageBucket: "volca-tools.firebasestorage.app",
+    messagingSenderId: "262525907487",
+    appId: "1:262525907487:web:4abc37fa3b583223569b2a"
+};
+
+firebase.initializeApp(firebaseConfig);
